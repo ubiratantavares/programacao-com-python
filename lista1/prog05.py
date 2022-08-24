dc1dea44aeb652f51346339d80d7275c3a150341
= <ust1973@gmail.com>
2022-08-24 01:28:06 +0000

    desenvolvimento do script prog05.py

--- lista1/prog05.py
+++ lista1/prog05.py
@@ -3,4 +3,18 @@ Ler dois valores para as variáveis A e B, e efetuar as trocas dos valores de fo
 da variável B e a variável B passe a possuir o valor da variável A. Apresentar os valores trocados.
 
 """
+a = int(input("Digite o valor de A: "))
+b = int(input("Digite o valor de B: "))
+
+# efetua a troca dos valores entre as variáveis
+aux = a
+a = b
+b = aux
+
+print("O valor de A: {}".format(a))
+print("O valor de B: {}".format(b))
+
+ 
+
+
 
