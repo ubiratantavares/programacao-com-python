b9eccf4ec1afaf1939df8d3b7cf6a6508667a498
= <ust1973@gmail.com>
2022-08-24 01:50:16 +0000

    desenvolvimento do script prog06.py

--- lista1/prog06.py
+++ lista1/prog06.py
@@ -3,4 +3,12 @@ Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenh
 
 """
 
+C = float(input("Digite a temperatura em graus Celsius: "))
+
+# converter a temperatura em graus Celsius para graus Fahrenheit
+F = (9 * C + 160) / 5.0
+
+print("Temperatura em graus Fahrenheit: {:.2f}".format(F))
+
+
 
